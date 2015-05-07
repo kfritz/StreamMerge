b2c239843e1e724f9428f9d53adf041e870e8b95
Kyle Peter Fritz <kyle.fritz@zocdoc.com>
2015-05-07 00:37:28 -0400

    Set up the webapi controller.

---
+++ src/StreamMerge/Controllers/QuizController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using StreamMerge.App.Quiz;
+using StreamMerge.App.Quiz.DataModel;
+
+namespace StreamMerge.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class QuizController : Controller
+    {
+        private readonly IStreamRepository _repository;
+
+        public QuizController(IStreamRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET quiz/merge?stream1=<stream_name_1>&stream2=<stream_name_2>
+        [HttpGet]
+        public async Task<StreamResponse> Merge(string stream1, string stream2)
+        {
+            var result = await _repository.GetNextAsync(stream1, stream2);
+            return result;
+        }
+    }
+}
--- src/StreamMerge/Startup.cs
+++ src/StreamMerge/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+using StreamMerge.App;
 
 namespace StreamMerge
 {
@@ -13,10 +14,13 @@ namespace StreamMerge
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddStreamMergeAppServices();
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMvc();
         }
     }
 }
