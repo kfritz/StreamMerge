9eb3dd0f6e21c28b7d1d468f32977debe81576c3
Kyle Peter Fritz <kyle.fritz@zocdoc.com>
2015-05-07 10:57:47 -0400

    Prepare fake data for tests.

---
+++ test/StreamMerge.App.Tests/Quiz/StreamRepositoryTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StreamMerge.App.Quiz;
+using StreamMerge.App.Quiz.DataModel;
+using Xunit;
+
+namespace StreamMerge.App.Tests.Quiz
+{
+    public class StreamRepositoryTests
+    {
+        [Fact]
+        public async Task GetNextAsync_MergesTwoStreams()
+        {
+
+        }
+
+        #region Fake Data Helpers
+
+        private IEnumerable<int> GetInfiniteSequence(int stepSize, int start = 0)
+        {
+            int current = start;
+            while(true)
+            {
+                yield return current;
+                current += stepSize;
+            }
+        }
+
+        private class FakeStreamClient : IStreamClient
+        {
+            private readonly IDictionary<string, IEnumerator<int>> _streams;
+
+            public FakeStreamClient(IDictionary<string, IEnumerator<int>> streams)
+            {
+                _streams = streams;
+            }
+
+            public Task<NamedStreamResponse> GetNextValueAsync(string streamName)
+            {
+                if(_streams.ContainsKey(streamName))
+                {
+                    var enumerator = _streams[streamName];
+                    var previous = enumerator.Current;
+                    enumerator.MoveNext();
+                    var next = enumerator.Current;
+
+                    return Task.FromResult(new NamedStreamResponse(streamName, next, previous));
+                }
+                else
+                {
+                    throw new ArgumentException("The given stream name was not setup for this test.", "streamName");
+                }
+            }
+        }
+
+        #endregion
+    }
+}
