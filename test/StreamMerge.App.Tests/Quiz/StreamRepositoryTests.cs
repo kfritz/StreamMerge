2d6fed3bcf715fe14a6ca2aafb1f421a3773b730
Kyle Peter Fritz <kyle.fritz@zocdoc.com>
2015-05-07 11:51:29 -0400

    Implement tests.

--- test/StreamMerge.App.Tests/Quiz/StreamRepositoryTests.cs
+++ test/StreamMerge.App.Tests/Quiz/StreamRepositoryTests.cs
@@ -13,7 +13,35 @@ namespace StreamMerge.App.Tests.Quiz
         [Fact]
         public async Task GetNextAsync_MergesTwoStreams()
         {
+            const int stream1StepSize = 3;
+            const int stream2StepSize = 7;
+            const string stream1Name = "stream1";
+            const string stream2Name = "stream2";
+            var stream1 = GetInfiniteSequence(stream1StepSize);
+            var stream2 = GetInfiniteSequence(stream2StepSize);
+            var streams = new Dictionary<string, IEnumerator<int>>(StreamNameComparer.Comparer)
+            {
+                { stream1Name, stream1.GetEnumerator() },
+                { stream2Name, stream2.GetEnumerator() }
+            };
 
+            var systemUnderTest = new StreamRepository(new FakeStreamClient(streams), new StreamStore());
+
+            const int numToCheck = 50;
+            int last = int.MinValue;
+            for (int index = 0; index < numToCheck; index++)
+            {
+                var next = await systemUnderTest.GetNextAsync(stream1Name, stream2Name);
+                Assert.True(next.CurrentValue >= last, "The next value from the stream should be greater than or equal to the previous one.");
+                Assert.True(
+                    next.CurrentValue % stream1StepSize == 0 || next.CurrentValue % stream2StepSize == 0, 
+                    "The values from the merged stream should come from either of the source streams.");
+                if (last != int.MinValue)
+                {
+                    Assert.True(last == next.LastValue, "The returned last value should match the previous invocation's current value.");
+                }
+                last = next.CurrentValue;
+            }
         }
 
         #region Fake Data Helpers
